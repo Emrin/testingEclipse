7de3366cf0f9b96d3b11fc50f1f302d10d39ef00
Emrin <emrin95@gmail.com>
2017-11-02 21:59:31 +0100

    cyka blyat

---
+++ src/myNameJeff/Main.java
@@ -0,0 +1,11 @@
+package myNameJeff;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// hello
+		System.out.println("Hello there.");
+	}
+
+}
