75f93bf45721830fe479c4c760ab10d728496654
Emrin <emrin95@gmail.com>
2017-11-08 18:29:14 +0100

    reb

--- src/myNameJeff/Main.java
+++ src/myNameJeff/Main.java
@@ -3,9 +3,8 @@ package myNameJeff;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// rebase
-		System.out.println("Hello there.");
+		// rebase
 	}
 
 }
