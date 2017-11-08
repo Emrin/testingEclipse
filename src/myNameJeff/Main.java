581e15a8e5ae55559e8efcd1a7cb67a8e4aabca5
Emrin <emrin95@gmail.com>
2017-11-08 18:23:45 +0100

    r

--- src/myNameJeff/Main.java
+++ src/myNameJeff/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// hello
-		// very nice
-		// branch2w
-		// updating only lilbranch with all the things in it
-		// now we rebase
+		// rebase
 		System.out.println("Hello there.");
 	}
 
