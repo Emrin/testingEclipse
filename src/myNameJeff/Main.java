bcaf4ce71c72efd4ba7c70d6acc7c65998379931
Emrin <emrin95@gmail.com>
2017-11-08 18:25:23 +0100

    master modified

--- src/myNameJeff/Main.java
+++ src/myNameJeff/Main.java
@@ -3,11 +3,7 @@ package myNameJeff;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// hello
-		// very nice
-		// branch2
-		System.out.println("Hello there.");
+		// master modified
 	}
 
 }
